1f621e50a0b59076f67af31505aba6054456d8be
Andy Dorman <andrew.dorman@piksel.com>
2015-10-16 09:04:45 +0100

    updated csv formatting

--- admin/index.php
+++ admin/index.php
@@ -105,9 +105,8 @@ $fh = fopen($fullPath, 'w');
     if($row['country']) {
       array_push($addressArr, $row['country']);
     }
-print_r($addressArr);
-echo $addressArr.implode(', ');
-    $address = $addressArr.implode(', ');
+
+    $address = implode(', ', $addressArr);
 
     $data .= $firstname."\t".$surname."\t".$row['dob']."\t".$address."\t".$row['tel']."\t".$row['email']."\t".preg_replace("/[\n\r]/", "", stripslashes($row['interests']))."\t".preg_replace("/[\n\r]/", "", stripslashes($row['suggestions']))."\t".$row['literature']."\t".$row['art']."\t".$row['languages']."\t".$row['fashion']."\t".$row['cosmetics']."\t".$row['spirituality']."\t".$row['self_development']."\t".$row['body_treatments']."\t".$row['skincare']."\t".$row['life_coaching']."\t".$row['facial_treatments']."\t".$row['alternative_therapies']."\t".$row['travel']."\t".$row['ayurveda']."\t".$row['lifestyle']."\t".$row['nutrition']."\t".$row['fitness']."\t".$row['theatre']."\t".$row['ballet']."\t".$row['socialising']."\t".$row['relaxing']."\t".$row['film_screenings']."\t".$row['yoga']."\t".$row['events']."\t".$row['created_at']."\n";
       }
