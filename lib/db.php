e738510211e5cad82243beb3ef0527413ee9b90f
Andy Dorman <andrew.dorman@piksel.com>
2016-01-05 01:08:11 +0100

    evnts managemnet

--- lib/db.php
+++ lib/db.php
@@ -2,10 +2,10 @@
 //ini_set('display_errors',1);
 //error_reporting(E_ALL);
 error_reporting(0);
-//$mysqli = new mysqli("127.0.0.1", "root", "", "gazelli-house", 3306);
+$mysqli = new mysqli("127.0.0.1", "root", "", "gazelli-house", 3306);
 //$mysqli = mysql_connect("127.0.0.1", "root", "", "gazelli-house");
 //$mysqli = mysql_connect("localhost", "gazelli-house", "g4z3ll1H0u53");
-$mysqli = mysql_connect("localhost", "root", "@ndyD0rm@n42");
+//$mysqli = mysql_connect("localhost", "root", "@ndyD0rm@n42");
 if (!$mysqli) {
     die('Could not connect: ' . mysql_error());
 }
