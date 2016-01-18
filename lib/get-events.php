1a35277dfcf7e21dd009ec2e81c942e415aa176e
Andy Dorman <andrew.dorman@piksel.com>
2016-01-18 20:00:08 +0000

    adding order by parametere at the end of any possible query for events

--- lib/get-events.php
+++ lib/get-events.php
@@ -3,10 +3,11 @@ require "../lib/db.php";
 $date = mysql_real_escape_string($_POST['date']);
 $parsedDate = date_parse($date);
 $month = $parsedDate["month"];
-$query = "SELECT * from events WHERE month(date) = '".$month."' ORDER BY date ASC";
+$query = "SELECT * from events WHERE month(date) = '".$month."'";
 if($_POST['active']) {
     $query .= " AND active=".mysql_real_escape_string($_POST['active']);
 }
+$query .= " ORDER BY date ASC";
 $out = array();
 if($result = mysql_query($query)) {
   while ($row = mysql_fetch_assoc($result)) {
