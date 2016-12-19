41f9eba926bdfab4db20da3c9da51d589ad29d0e
Andy Dorman <andrew.dorman@piksel.com>
2016-12-19 14:25:12 +0000

    adding explore page

---
+++ explore.php
@@ -0,0 +1,319 @@
+<?php
+require 'lib/config.php';
+require 'lib/Mobile_Detect.php';
+$detect = new Mobile_Detect;
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Meta, title, CSS, favicons, etc. -->
+    <meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="description" content="Gazelli House, launching in South Kensington in London 2015, brings a new and innovative concept to the beauty industry.  Based on a foundation of unparalleled expertise in skincare, Gazelli is expanding in to lifestyle where all areas of well-being are addressed with a dynamic calendar of events and a team of specialist practitioners.">
+<meta name="keywords" content="skincare, targeted, treatments, lifting, sculpting, formulations, acne, skin, ageing, anti-ageing, youth, wrinkles, fine lines, lymphatic, Gazelli, Dr Hamzayeva, experts, specialist, Askarova, Azerbaijan, Baku, Oxyjet, Radiofrequency, Microneedling, MesoVytal, Microdermabrasion, High frequency, LED, light therapy, health, body, mind, soul, mindfulness, nutrition, lifestyle, programme, discover, learn, create, hypnotherapy, life coach, counselling, nlp, cbt, sound therapy, art therapy, ayuverda, wellbeing, wellness, emotional intelligence, advice, mentor, journey, spirit, spiritual, yoga">
+<meta name="author" content="">
+<title>Gazelli House - Life</title>
+
+<!-- Bootstrap core CSS -->
+<link href="/bower_components/bootstrap-calendar/css/calendar.css" rel="stylesheet">
+<link href="stylesheets/style.css?v=2.0" rel="stylesheet">
+<link href="stylesheets/jquery.fancybox.css" rel="stylesheet">
+<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+<!--[if lt IE 9]>
+  <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+<![endif]-->
+
+<!-- Favicons -->
+<link rel="apple-touch-icon-precomposed" href="/apple-touch-icon-precomposed.png">
+<link rel="icon" href="/favicon.ico">
+
+  </head>
+  <body class="life-page">
+  <a href="#" name="top" id="top"></a>
+    <!-- Docs master nav -->
+    <header class="navbar navbar-static-top text-center" id="top" role="banner">
+      <div class="navbar-header">
+        <button class="navbar-toggle" type="button" data-toggle="collapse" data-target=".bs-navbar-collapse">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a href="../" class="navbar-brand">Gazelli House</a>
+      </div>
+      <nav class="collapse navbar-collapse bs-navbar-collapse" role="navigation">
+        <ul class="nav navbar-nav">
+          <li><a href="/">Home</a></li>
+          <li><a href="/menu">Treatments</a></li>
+          <li><a href="/events">Events</a></li>
+          <li><a href="/explore">Explore</a></li>
+          <li><a href="/membership">Registration</a></li>
+          <li><a href="https://gazellihouse.phorest.me/book#/service-category?id=9CBphLQfcl9L8yTTnT5Q9Q" target="_blank">Book now</a></li>
+        </ul>
+        <ul class="nav navbar-nav navbar-right">
+        </ul>
+      </nav>
+    </header>
+    <div class="container" id="life">
+      <a class="sr-only sr-only-focusable" href="#content">Skip to main content</a>
+      <div>
+        <!-- Page content of course! -->
+        <main id="content" role="main">
+
+          <div class="mood-board one">
+            <img data-src="/images/life/life_1.jpg" class="lazy" src="/images/blank.gif" width="690" height="665"/>
+            <div class="textbox">
+              <div>
+                <h4>Welcome to the Gazelli House...</h4>
+                <p>
+                  Read on to explore our three light-filled floors, discover our range of services and engaging activities, and be inspired by how much can be achieved in a day.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="mood-board two">
+            <img data-src="/images/life/life_2.jpg" class="lazy" src="/images/blank.gif" width="690" height="527"/>
+            <div class="textbox">
+              <div>
+                <h4>9am - Flower Arrival</h4>
+                <p>
+                  <span>Our vibrant living space</span><span> is an uplifting place to begin your morning. Here, you will find vases of carefully arranged flowers, flickering tea lights and delicious infused water, as well as our friendly house coordinators, ready to help you map out your journey.</span>
+                </p>
+                <a href="#" class="toggle"></a>
+              </div>
+            </div>
+          </div>
+
+          <div class="mood-board three">
+            <img data-src="/images/life/life_3.jpg" class="lazy" src="/images/blank.gif" width="690" height="394"/>
+            <div class="textbox">
+              <div>
+                <h4>10am - Plan your day</h4>
+                <p>
+                <span>Enjoy a freshly brewed coffee</span><span> by the fireplace. Daily newspapers, novels and books are strewn across the table, for you to pick up, read, and contemplate.</span>
+                </p>
+                <a href="#" class="toggle"></a>
+              </div>
+            </div>
+          </div>
+
+        <div class="mood-board four">
+          <img data-src="/images/life/life_4.jpg" class="lazy" src="/images/blank.gif" width="690" height="784"/>
+          <div class="textbox">
+            <div>
+              <h4>11am - Skin Expertise</h4>
+              <p>
+              <span>Our prescriptive treatments</span><span> address all skincare concerns. Hand-blended products, rich in our signature White Oil, are coupled with bespoke massage techniques and results-driven technology: designed to heal, and bring harmony to the skin.</span>
+              </p>
+              <a href="#" class="toggle"></a>
+            </div>
+          </div>
+        </div>
+
+        <div class="mood-board five">
+          <img data-src="/images/life/life_5.jpg" class="lazy" src="/images/blank.gif" width="690" height="430"/>
+          <div class="textbox">
+            <div>
+              <h4>2:30pm - A Place To Reflect</h4>
+              <p>
+              <span>Take time out of your day</span><span> to sit by yourself, or write in a notebook. Get lost in a book and let your senses awaken.</span>
+              </p>
+              <a href="#" class="toggle"></a>
+            </div>
+          </div>
+          <div class="textbox">
+            <div>
+              <h4>3:30pm - Discover New Hobbies</h4>
+              <p>
+              <span>Meet like-minded people</span><span> through engaging activities, poetry readings, life drawing classes and informal workshops that encourage new ways of thinking, unlocking your creativity.</span>
+              </p>
+              <a href="#" class="toggle"></a>
+            </div>
+          </div>
+        </div>
+
+        <div class="mood-board six">
+          <img data-src="/images/life/life_6.jpg" class="lazy" src="/images/blank.gif" width="690" height="480"/>
+          <div class="textbox">
+            <div>
+              <h4>5:00pm - Our House Garden</h4>
+              <p>
+              <span>Our garden comes to life</span><span> in the summer. It is a meditative spot to unwind with a glass of wine, to be in the company of friends,
+              or simply to enjoy the fresh air or a morning yoga class.</span>
+              </p>
+              <a href="#" class="toggle"></a>
+            </div>
+          </div>
+        </div>
+
+        <div class="mood-board seven">
+          <img data-src="/images/life/life_7.jpg" class="lazy" src="/images/blank.gif" width="690" height="662"/>
+          <div class="textbox">
+            <div>
+              <h4>7:00pm - Evenings at Gazelli House</h4>
+              <p>
+                <span>Human interaction is at the heart</span><span> of the Gazelli philosophy. Our evening events are a way to meet new friends, hear live music, and be sociable. They speak to all ages, backgrounds and minds.<span>
+              </p>
+              <a href="#" class="toggle"></a>
+            </div>
+          </div>
+        </div>
+
+          <hr/>
+          <div class="text-center footer">
+            <p>Gazelli House | 174 Walton Street | London | SW3 2JL<br/>
+              TO BOOK ONLINE <a href="https://gazellihouse.phorest.me/book#/service-category?id=9CBphLQfcl9L8yTTnT5Q9Q" target="_blank">CLICK HERE</a>
+            </p>
+          </div>
+        </main>
+
+        <?php
+        if($detect->isMobile()) {
+          $limit = 24;
+        } else {
+          $limit = 100;
+        }
+        ?>
+      </div>
+      <div class="social-icons">
+        <ul class="list-inline text-center">
+            <li class="social-icon bird"><a href="https://www.twitter.com/gazellihouse" target="_blank"></a></li>
+            <li class="social-icon facebook"><a href="https://www.facebook.com/GazelliHouse" target="_blank"></a></li>
+            <li class="social-icon icon-3"><a href="https://www.instagram.com/gazellihouse" target="_blank"></a></li>
+            <li class="social-icon twitter"><a href="http://gazellihouselondon.tumblr.com" target="_blank"></a></li>
+          </ul>
+      </div>
+    </div>
+<!-- Bootstrap core JavaScript
+================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+<script src="javascripts/gazelli-house.min.js"></script>
+
+<?php
+if($detect->isMobile() || $detect->isTablet()) {
+?>
+<script type="text/javascript">
+function mobileclickToHover() {
+  $('.key-panel.goal').each(function(index, value) {
+    $(value).unbind("click");
+    $(value).on("click", function(){
+      $('.key-panel.goal').removeClass("hover");
+      $(this).addClass("hover");
+    });
+  });
+}
+$(document).ready(function(){
+  mobileclickToHover();
+});
+</script>
+<?php
+}
+?>
+    <script>
+      window.twttr = (function (d,s,id) {
+        var t, js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return; js=d.createElement(s); js.id=id; js.async=1;
+        js.src="https://platform.twitter.com/widgets.js"; fjs.parentNode.insertBefore(js, fjs);
+        return window.twttr || (t = { _e: [], ready: function(f){ t._e.push(f) } });
+      }(document, "script", "twitter-wjs"));
+    </script>
+    <script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+                    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+          ga('create', 'UA-48048293-2', 'auto');
+          ga('send', 'pageview');
+    </script>
+    <script>
+    /* lazyload.js (c) Lorenzo Giuliani
+     * MIT License (http://www.opensource.org/licenses/mit-license.html)
+     *
+     * expects a list of:
+     * `<img src="blank.gif" data-src="my_image.png" width="600" height="400" class="lazy">`
+     */
+
+    !function(window){
+
+      var $q = function(q, res){
+            if (document.querySelectorAll) {
+              res = document.querySelectorAll(q);
+            } else {
+              var d=document
+                , a=d.styleSheets[0] || d.createStyleSheet();
+              a.addRule(q,'f:b');
+              for(var l=d.all,b=0,c=[],f=l.length;b<f;b++)
+                l[b].currentStyle.f && c.push(l[b]);
+
+              a.removeRule(0);
+              res = c;
+            }
+            return res;
+          }
+        , addEventListener = function(evt, fn){
+            window.addEventListener
+              ? this.addEventListener(evt, fn, false)
+              : (window.attachEvent)
+                ? this.attachEvent('on' + evt, fn)
+                : this['on' + evt] = fn;
+          }
+        , _has = function(obj, key) {
+            return Object.prototype.hasOwnProperty.call(obj, key);
+          }
+        ;
+
+      function loadImage (el, fn) {
+        var img = new Image()
+          , src = el.getAttribute('data-src');
+        img.onload = function() {
+          if (!! el.parent)
+            el.parent.replaceChild(img, el)
+          else
+            el.src = src;
+
+          fn? fn() : null;
+        }
+        img.src = src;
+      }
+
+      function elementInViewport(el) {
+        var rect = el.getBoundingClientRect(),
+        bodyRect = document.body.getBoundingClientRect();
+        return (
+           (rect.top    >= 0
+        && rect.left   >= 0
+        && rect.top <= (window.innerHeight || document.documentElement.clientHeight)) ||
+        (el.offsetParent.offsetTop) + el.height > window.scrollY
+        )
+      }
+
+        var images = new Array()
+          , query = $q('img.lazy')
+          , processScroll = function(){
+              for (var i = 0; i < images.length; i++) {
+                if (elementInViewport(images[i])) {
+                  loadImage(images[i], function () {
+                    images.splice(i, i);
+                  });
+                }
+              };
+            }
+          ;
+        // Array.prototype.slice.call is not callable under our lovely IE8
+        for (var i = 0; i < query.length; i++) {
+          images.push(query[i]);
+        };
+        $('.toggle').on('click', function (e) {
+          e.preventDefault();
+          $(this).parent().toggleClass('open');
+        })
+        processScroll();
+        addEventListener('scroll', processScroll);
+    }(this)
+    </script>
+  </body>
+</html>
